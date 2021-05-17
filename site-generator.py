5cc60f20aec6c33c54cff8bb77a80e57044f4349
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2021-05-17 21:55:46 +0545

    Update site-generator.py

--- site-generator.py
+++ site-generator.py
@@ -41,7 +41,11 @@ def generate_middle(name: str, link: str, students: list) -> str:
 for_middle = [generate_middle(project['name'], project['link'], project['by'].values()) for project in projects.values()]
 
 with open("html/c-projects.html", "w") as html_file:
-    html_file.write(initial)
+    #html_file.write(initial)
+    sys.stdout.write(initial)
     for each_middle in for_middle:
-        html_file.write(each_middle)
-    html_file.write(final)
+        #html_file.write(each_middle)
+        sys.stdout.write(each_middle)
+    #html_file.write(final)
+    sys.stdout.write(final)
+ 
